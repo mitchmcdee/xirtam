f0946890950ae885b812b62bf4922a869d4076a3
mitchmcdee <mitchmcdee@gmail.com>
2018-11-03 20:02:13 +1000

    added fire modules to timtamnet. Reduced paramters to ~900 and increased layers to 22.

--- xirtam/neural/timtamnet.py
+++ xirtam/neural/timtamnet.py
@@ -1,8 +1,18 @@
 """
 Module containing TimTamNet.
 """
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
-from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, concatenate
+from keras.models import Input, Model
+
+
+def fire_module(x, num_squeeze_filters, num_expand_filters):
+    """
+    Fire module from SqueezeNet.
+    """
+    x = Conv2D(num_squeeze_filters, (1, 1), activation="relu")(x)
+    left = Conv2D(num_expand_filters, (1, 1), activation="relu")(x)
+    right = Conv2D(num_expand_filters, (3, 3), padding='same', activation="relu")(x)
+    return concatenate([left, right])
 
 
 def TimTamNet(input_shape=(128, 128, 1)):
@@ -10,18 +20,19 @@ def TimTamNet(input_shape=(128, 128, 1)):
     TimTamNet is a Fully Convolutional Network (FCN) designed to learn segmented
     regions of an image.
     """
-    timtamnet = Sequential(name="timtamnet")
-    timtamnet.add(Conv2D(4, (1, 1), padding="same", activation="relu", input_shape=input_shape))
-    timtamnet.add(Conv2D(4, (3, 3), padding="same", activation="relu"))
-    timtamnet.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    timtamnet.add(Conv2D(8, (1, 1), padding="same", activation="relu"))
-    timtamnet.add(Conv2D(8, (3, 3), padding="same", activation="relu"))
-    timtamnet.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    timtamnet.add(Conv2D(8, (1, 1), padding="same", activation="relu"))
-    timtamnet.add(Conv2D(8, (3, 3), padding="same", activation="relu"))
-    timtamnet.add(UpSampling2D((2, 2)))
-    timtamnet.add(Conv2D(4, (1, 1), padding="same", activation="relu"))
-    timtamnet.add(Conv2D(4, (3, 3), padding="same", activation="relu"))
-    timtamnet.add(UpSampling2D((2, 2)))
-    timtamnet.add(Conv2D(1, (1, 1), padding="same", activation="sigmoid"))
-    return timtamnet
+    inputs = Input(shape=input_shape)
+    x = fire_module(inputs, 2, 4)
+    x = MaxPooling2D((2, 2))(x)
+    x = fire_module(x, 2, 4)
+    x = MaxPooling2D((2, 2))(x)
+    x = fire_module(x, 4, 4)
+    x = MaxPooling2D((2, 2))(x)
+    x = fire_module(x, 2, 4)
+    x = UpSampling2D((2, 2))(x)
+    x = fire_module(x, 4, 4)
+    x = UpSampling2D((2, 2))(x)
+    x = fire_module(x, 2, 4)
+    x = UpSampling2D((2, 2))(x)
+    x = fire_module(x, 2, 4)
+    x = Conv2D(1, (1, 1), activation="sigmoid")(x)
+    return Model(inputs, x, name='timtamnet')
